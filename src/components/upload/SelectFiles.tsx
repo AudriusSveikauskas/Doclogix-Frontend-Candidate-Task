428138ff18aeb7c7a87e7ca56181b0ee55af662f
Audrius Sveikauskas <audrius@sveikauskas.dev>
2023-02-21 14:15:41 +0200

    feat: max files upload function added

--- src/components/upload/SelectFiles.tsx
+++ src/components/upload/SelectFiles.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Box, Typography } from '@mui/material';
 import { useDispatch, useSelector } from 'react-redux';
+import { useSnackbar, VariantType } from 'notistack';
 import UploadImg from '../../assets/upload-files.jpg';
 import { fileActions } from '../../store/file/file';
 import setFileType from '../../utils/setFileType';
@@ -14,8 +15,9 @@ const SelectFiles = () => {
   const [acceptType, setAcceptType] = useState('application/pdf');
 
   const dispatch = useDispatch();
+  const { enqueueSnackbar } = useSnackbar();
 
-  const MAX_COUNT = 10;
+  const MAX_COUNT = 5;
 
   const uploadedFilesProps = useSelector<RootState, IDocDetails[]>(
     (state) => state.file.uploadedFilesProps,
@@ -29,6 +31,15 @@ const SelectFiles = () => {
     dispatch(fileActions.setUploadedFilesProps(filesArr));
   };
 
+  const handleAlert = (msgVariant: VariantType, msgDesc: string) => {
+    enqueueSnackbar(msgDesc, {
+      variant: msgVariant,
+      preventDuplicate: true,
+      autoHideDuration: 4000,
+      anchorOrigin: { horizontal: 'right', vertical: 'bottom' },
+    });
+  };
+
   const handleUploadFiles = (files: File[]) => {
     const uploaded = [...uploadedFiles];
     let limitExceeded = false;
@@ -38,7 +49,6 @@ const SelectFiles = () => {
         uploaded.push(file);
         if (uploaded.length === MAX_COUNT) setFileLimit(true);
         if (uploaded.length > MAX_COUNT) {
-          alert(`You can only add a maximum of ${MAX_COUNT} files`);
           setFileLimit(false);
           limitExceeded = true;
           return true;
@@ -51,6 +61,12 @@ const SelectFiles = () => {
   const handleFileEvent = (e: React.ChangeEvent<HTMLInputElement>) => {
     const chosenFiles = Array.prototype.slice.call(e.target.files);
     handleUploadFiles(chosenFiles);
+  };
+
+  const handleClick = () => {
+    if (fileLimit) {
+      handleAlert('error', `You can only add a maximum of ${MAX_COUNT} files`);
+    }
   };
 
   useEffect(() => {
@@ -74,6 +90,10 @@ const SelectFiles = () => {
     if (uploadedFilesProps.length === 0) {
       setUploadedFiles([]);
     }
+
+    if (uploadedFilesProps.length < MAX_COUNT) {
+      setFileLimit(false);
+    }
   }, [uploadedFilesProps]);
 
   useEffect(() => {
@@ -88,6 +108,7 @@ const SelectFiles = () => {
 
   return (
     <Box
+      onClick={handleClick}
       sx={{
         display: 'flex',
         flexDirection: 'column',
@@ -115,23 +136,26 @@ const SelectFiles = () => {
         <img src={UploadImg} alt="Select files" />
       </Box>
 
-      <input
-        id="fileUpload"
-        type="file"
-        multiple
-        accept={acceptType}
-        onChange={handleFileEvent}
-        disabled={fileLimit}
-        title=" "
-        style={{
-          position: 'absolute',
-          top: '0px',
-          left: '0px',
-          width: '100%',
-          height: '100%',
-          opacity: 0,
-        }}
-      />
+      <Box display={!fileLimit ? 'block' : 'none'}>
+        <input
+          id="fileUpload"
+          type="file"
+          multiple
+          accept={acceptType}
+          onChange={handleFileEvent}
+          disabled={fileLimit}
+          title=" "
+          style={{
+            position: 'absolute',
+            top: '0px',
+            left: '0px',
+            width: '100%',
+            height: '100%',
+            opacity: 0,
+          }}
+        />
+      </Box>
+
     </Box>
   );
 };
