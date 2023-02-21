219a7013f667b41d34b45950b370206a851e5c81
Audrius Sveikauskas <audrius@sveikauskas.dev>
2023-02-21 16:55:37 +0200

    fixed main tab issue

--- src/layouts/UserLayout.tsx
+++ src/layouts/UserLayout.tsx
@@ -1,18 +1,25 @@
 import React, { useEffect } from 'react';
 import { Outlet, useNavigate } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '../store/store';
+import { menuActions } from '../store/menu/menu';
 
 const UserLayout = () => {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   const isAuthenticated = useSelector<RootState, boolean>(
     (state) => state.auth.isAuthenticated,
   );
 
+  const setSelectedTab = (title: string) => {
+    dispatch(menuActions.setSelectedTab(title));
+  };
+
   useEffect(() => {
     if (!isAuthenticated) {
       navigate('/sign-in');
+      setSelectedTab('Upload');
     }
   }, [isAuthenticated]);
 
--- src/store/menu/menu.ts
+++ src/store/menu/menu.ts
@@ -1,7 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialMenu = {
-  selectedTab: '',
+  selectedTab: 'Upload',
   isDrawerOpen: false,
 };
 
